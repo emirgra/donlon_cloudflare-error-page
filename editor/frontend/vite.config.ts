80100677b2c82d3fb07929575f1a0df9c176a8ab
Anthony Donlon <idonlon@hotmail.com>
2025-12-10 00:30:35 +0800

    editor/frontend: fix resources path

--- editor/frontend/vite.config.ts
+++ editor/frontend/vite.config.ts
@@ -1,36 +1,43 @@
-import { defineConfig } from 'vite';
+/// <reference types="vite/types/importMeta.d.ts" />
+
+import { defineConfig, loadEnv } from 'vite';
 import { minify as htmlMinify } from 'html-minifier-terser';
+import process from 'node:process';
 
-export default defineConfig({
-  appType: 'mpa',
-  base: '/editor/',
-  build: {
-    minify: true,
-    sourcemap: true,
-  },
-  server: {
-    port: 3000,
-    proxy: {
-      '/s': {
-        target: 'http://localhost:5000',
-      },
+
+export default defineConfig(({ mode }) => {
+  const baseUrl = mode === 'production' ? '' : '/editor/';
+  return {
+    appType: 'mpa',
+    base: baseUrl,
+    build: {
+      minify: true,
+      sourcemap: true,
     },
-  },
-
-  plugins: [
-    {
-      name: 'html-minifier',
-      transformIndexHtml: {
-        order: 'post',
-        handler(html) {
-          return htmlMinify(html, {
-            collapseWhitespace: true,
-            removeComments: true,
-            minifyCSS: true,
-            minifyJS: true,
-          });
+    server: {
+      port: 3000,
+      proxy: {
+        '/s': {
+          target: 'http://localhost:5000',
         },
       },
     },
-  ],
+
+    plugins: [
+      {
+        name: 'html-minifier',
+        transformIndexHtml: {
+          order: 'post',
+          handler(html) {
+            return htmlMinify(html, {
+              collapseWhitespace: true,
+              removeComments: true,
+              minifyCSS: true,
+              minifyJS: true,
+            });
+          },
+        },
+      },
+    ],
+  };
 });
